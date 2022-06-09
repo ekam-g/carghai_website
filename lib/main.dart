e377ed83180abffa12d15abd0ddefd0a62e0db9f
Carghai74 <94583299+carghai@users.noreply.github.com>
2022-06-09 12:30:30 -0400

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:carghai_website/screens/home.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Carghai's Website",
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        backgroundColor: Colors.black,
+        scaffoldBackgroundColor: Colors.black,
+      ),
+      home: const MyHomePage(title: "Carghai's Website"),
+    );
+  }
+}
---
+++ lib/screens/home.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+        titleTextStyle: const TextStyle(
+          color: Colors.black,
+          fontSize: 20,
+          fontWeight: FontWeight.bold,
+        ),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: const <Widget>[
+            Text(
+              'You have pushed the button this many times:',
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+                color: Colors.white,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
