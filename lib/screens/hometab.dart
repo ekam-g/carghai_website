c4e2e8f278801ed14688cf05e170272ffd951574
Carghai74 <94583299+carghai@users.noreply.github.com>
2022-06-16 17:44:18 -0400

    resize

--- lib/screens/hometab.dart
+++ lib/screens/hometab.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:auto_size_text/auto_size_text.dart';
 
 class tab1Home extends StatefulWidget {
   const tab1Home({
@@ -21,8 +22,10 @@ class _tab1Home extends State<tab1Home> {
         ),
         const Padding(
           padding: EdgeInsets.symmetric(horizontal: 20, vertical: 5),
-          child: Text(
-            'Hello, I am Carghai and I am a software developer who is mainly codes in python and dart.',
+          child: AutoSizeText(
+            'Hello, I am Carghai and I am a software developer who is mainly codes in python and dart.\nI love building thing that I can use and help other people with(example show below).',
+            textAlign: TextAlign.center,
+            maxLines: 2,
             style: TextStyle(
               fontSize: 20,
               fontWeight: FontWeight.bold,
@@ -34,19 +37,31 @@ class _tab1Home extends State<tab1Home> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             const Spacer(),
-            const Text(
-              "hello",
-              style: TextStyle(fontSize: 20),
+            const Expanded(
+              flex: 8,
+              child: SizedBox(
+                height: 200,
+                child: AutoSizeText(
+                  "I have built in flutter Cougar General for my school. So what does it do? It is an app to many things. The app has 2 main features. Stop it and cougar help."
+                  "Stop it is an app to stop passive types of bullying. It has a detect mode that listens to your voice and if you say something bad it will notify you. It has a learning ai with firestore to adapt and learn."
+                  "Next We have Cougar General. It is an app to help you with many things mainly during robotics events.It helps by telling you info on the event and it can be updated by firestore. It also uses push notification to store the data and soon as it comes so conserve on wifi(wifi is very bad at events)",
+                  style: TextStyle(fontSize: 20),
+                  maxLines: 10,
+                  textAlign: TextAlign.center,
+                ),
+              ),
             ),
             const Spacer(flex: 2),
-            SizedBox(
-              height: 200,
-              width: 200,
-              child: SingleChildScrollView(
-                child: Column(
-                  children: const [
-                    Text("cool", style: TextStyle(fontSize: 299)),
-                  ],
+            Expanded(
+              flex: 8,
+              child: SizedBox(
+                height: 200,
+                child: SingleChildScrollView(
+                  child: Column(
+                    children: const [
+                      Text("cool", style: TextStyle(fontSize: 299)),
+                    ],
+                  ),
                 ),
               ),
             ),
