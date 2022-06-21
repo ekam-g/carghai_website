0198d3b9bb2d7a3ccfbec303b19f3a2c2e1fe83a
Carghai74 <94583299+carghai@users.noreply.github.com>
2022-06-21 11:08:39 -0400

    project.dart is done?

--- lib/screens/hometab.dart
+++ lib/screens/hometab.dart
@@ -13,57 +13,91 @@ class tab1Home extends StatefulWidget {
 class _tab1Home extends State<tab1Home> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        body: Center(
-            child: Column(
-      children: [
-        const Spacer(
-          flex: 1,
-        ),
-        const Padding(
-          padding: EdgeInsets.symmetric(horizontal: 100, vertical: 0),
-          child: AutoSizeText(
+    var screenSize = MediaQuery.of(context).size.width;
+
+    ///BIG UI
+    if (screenSize > 600) {
+      return Scaffold(
+          body: Center(
+              child: Column(
+        children: [
+          const Spacer(
+            flex: 1,
+          ),
+          const AutoSizeText(
             'E k a m  G h a i',
             textAlign: TextAlign.start,
+            style: TextStyle(fontSize: 100, fontFamily: 'Lato'),
             maxLines: 1,
-            style: TextStyle(fontSize: 125, fontFamily: 'Lato'),
           ),
-        ),
-        const Padding(
-          padding: EdgeInsets.symmetric(horizontal: 100, vertical: 0),
-          child: AutoSizeText(
-            'A Full-Stack Dart and Python Developer',
+          const AutoSizeText(
+            'A full stack dart and python developer',
             textAlign: TextAlign.start,
-            maxLines: 1,
             style: TextStyle(fontSize: 20, fontFamily: 'Lato'),
+            maxLines: 1,
           ),
-        ),
-        const Spacer(
-          flex: 1,
-        ),
-        Expanded(
-          flex: 10,
-          child: Image.asset('assets/flutter cool.jpg'),
-        ),
-        const Spacer(
-          flex: 2,
-        ),
-        const Padding(
-          padding: EdgeInsets.symmetric(horizontal: 100,),
-          child: SizedBox(
-            width: 400,
-            child: AutoSizeText(
-              'I build things with a purpose. All of my projects are build to solve a problem or a problem that I have in mind. I love to learn new things and I love to help people and my projects reflect that.',
-              textAlign: TextAlign.center,
-              maxLines: 5,
-              style: TextStyle(fontSize: 20, fontFamily: 'Lato'),
-            ),
+          const Spacer(
+            flex: 1,
           ),
-        ),
-        const Spacer(
-          flex: 4,
-        ),
-      ],
-    )));
+          Expanded(
+            flex: 10,
+            child: Image.asset('assets/flutter cool.jpg'),
+          ),
+          const Spacer(
+            flex: 2,
+          ),
+          const AutoSizeText(
+            'I build things with a purpose.\n All of my projects are build to solve a problem or a problem that I have in mind.\nI love to learn new things and I love to help people and my projects reflect that.',
+            textAlign: TextAlign.center,
+            style: TextStyle(fontSize: 15, fontFamily: 'Lato'),
+            maxLines: 3,
+          ),
+          const Spacer(
+            flex: 4,
+          ),
+        ],
+      )));
+
+      ///SMALL UI
+    } else {
+      return Scaffold(
+          body: Center(
+              child: Column(
+        children: [
+          const Spacer(
+            flex: 1,
+          ),
+          const Text(
+            'E k a m  G h a i',
+            textAlign: TextAlign.start,
+            style: TextStyle(fontSize: 50, fontFamily: 'Lato'),
+          ),
+          const Text(
+            'A full stack dart and python developer small',
+            textAlign: TextAlign.start,
+            style: TextStyle(fontSize: 10, fontFamily: 'Lato'),
+          ),
+          const Spacer(
+            flex: 1,
+          ),
+          SizedBox(
+            width: 220,
+            height: 220,
+            child: Image.asset('assets/flutter cool.jpg'),
+          ),
+          const Spacer(
+            flex: 2,
+          ),
+          const Text(
+            'I build things with a purpose.\n All of my projects are build to solve a problem or a problem that I have in mind.\nI love to learn new things and I love to help people and my projects reflect that.',
+            textAlign: TextAlign.center,
+            style: TextStyle(fontSize: 8, fontFamily: 'Lato'),
+          ),
+          const Spacer(
+            flex: 4,
+          ),
+        ],
+      )));
+    }
   }
 }
--- lib/screens/projects.dart
+++ lib/screens/projects.dart
@@ -1,6 +1,4 @@
 // ignore_for_file: library_private_types_in_public_api, camel_case_types
-
-import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/material.dart';
 
 class projectsTab extends StatefulWidget {
@@ -20,13 +18,15 @@ class _projectsTab extends State<projectsTab> {
             child: Column(
       children: [
         const Spacer(),
-        const Padding(
-          padding: EdgeInsets.symmetric(horizontal: 50, vertical: 0),
-          child: AutoSizeText(
-            'P r o j e c t s',
-            textAlign: TextAlign.start,
-            maxLines: 1,
-            style: TextStyle(fontSize: 50, fontFamily: 'Lato'),
+        const Expanded(
+          flex: 6,
+          child: FittedBox(
+            fit: BoxFit.contain,
+            child: Text(
+              'P r o j e c t s',
+              textAlign: TextAlign.start,
+              style: TextStyle(fontSize: 150, fontFamily: 'Lato'),
+            ),
           ),
         ),
         const Padding(
@@ -44,22 +44,24 @@ class _projectsTab extends State<projectsTab> {
                 flex: 10,
               ),
               const Expanded(
-                flex: 15,
-                child: AutoSizeText(
-                  'Cougar Scouting App is an app to gather and find the best team that we can team up with. It is a mobile app that is built with Flutter and Firebase. It is a full-stack app that is built with Dart and Python.',
-                  textAlign: TextAlign.center,
-                  maxLines: 5,
-                  style: TextStyle(fontSize: 50, fontFamily: 'Lato'),
+                flex: 6,
+                child: FittedBox(
+                  fit: BoxFit.contain,
+                  child: Text(
+                    'Cougar Scouting App is an app to gather and find the best team that we can team up with. It is a mobile app that is built with Flutter and Firebase. It is a full-stack app that is built with Dart and Python',
+                    textAlign: TextAlign.start,
+                    style: TextStyle(fontSize: 150, fontFamily: 'Lato'),
+                  ),
                 ),
               ),
               Expanded(
                 flex: 15,
                 child: Container(
                   height: 300,
-                  decoration:  const BoxDecoration(
+                  decoration: const BoxDecoration(
                     shape: BoxShape.circle,
                     color: Colors.white,
-                    image:  DecorationImage(
+                    image: DecorationImage(
                       image: AssetImage('assets/1403logo.png'),
                     ),
                   ),
@@ -84,18 +86,20 @@ class _projectsTab extends State<projectsTab> {
               ),
               const Expanded(
                 flex: 15,
-                child: AutoSizeText(
-                  'Cougar General is an app made to house all of 1403 app projects to help the team out. It has two big features, Cougar help and Stop It. Cougar help is an app that helps with events and team origination in poor wifi. Stop it is anti bullying app that listens for passive type of harm and reports it. It has learning AI to learn to new type of passive harm.',
-                  textAlign: TextAlign.center,
-                  maxLines: 8,
-                  style: TextStyle(fontSize: 50, fontFamily: 'Lato'),
+                child: FittedBox(
+                  fit: BoxFit.contain,
+                  child: Text(
+                    'Cougar General is an app made to house all of 1403 app projects to help the team out.\n It has two big features, Cougar help and Stop It.\nCougar help is an app that helps with events and team origination in poor wifi.\n Stop it is anti bullying app that listens for passive type of harm and reports it.\n It has learning AI to learn to new type of passive harm.',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(fontSize: 100, fontFamily: 'Lato'),
+                  ),
                 ),
               ),
               Expanded(
                 flex: 15,
                 child: Container(
                   height: 300,
-                  decoration:  const BoxDecoration(
+                  decoration: const BoxDecoration(
                     shape: BoxShape.circle,
                     color: Colors.white,
                     image: DecorationImage(
@@ -122,19 +126,21 @@ class _projectsTab extends State<projectsTab> {
                 flex: 10,
               ),
               const Expanded(
-                flex: 15,
-                child: AutoSizeText(
-                  'NeuraCode is a teaching business where we expand our knowledge and teach kids to become future programmers. It is complete up and is a full running company.',
-                  textAlign: TextAlign.center,
-                  maxLines: 5,
-                  style: TextStyle(fontSize: 50, fontFamily: 'Lato'),
+                flex: 6,
+                child: FittedBox(
+                  fit: BoxFit.contain,
+                  child: Text(
+                    'NeuraCode is a teaching business where we expand our knowledge and teach kids to become future programmers. It is complete up and is a full running company.',
+                    textAlign: TextAlign.start,
+                    style: TextStyle(fontSize: 150, fontFamily: 'Lato'),
+                  ),
                 ),
               ),
               Expanded(
                 flex: 15,
                 child: Container(
                   height: 300,
-                  decoration:  const BoxDecoration(
+                  decoration: const BoxDecoration(
                     shape: BoxShape.circle,
                     color: Colors.white,
                     image: DecorationImage(
